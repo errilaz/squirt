a6d9643c9eb52ca85263d059a24618a87e0af470
Errilaz <errilaz@errilaz.org>
2023-03-06 21:46:14 +0100

    remove unneeded comment

--- packages/example/src/common/site.global.ts
+++ packages/example/src/common/site.global.ts
@@ -1,9 +1,3 @@
-/**
- * Waiting on possible indented application in civet, these
- * helpers make deeply nested markup a little easier on
- * the eyes (https://github.com/DanielXMoore/Civet/pull/398)
- */
-
 declare global {
   const siteName: string
 }
